a8c296943b24dad0672332ef869efe482f9faace
mikelyt777 <mikelyt777@users.noreply.github.com>
2020-06-07 13:41:38 +0800

    Update 2020-06-07T05:41:25.777Z

--- app/assets/js/all.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-
-  $('.burger').click(function(e) {
-    e.preventDefault();
-    $('.menu').toggleClass('active')
-  })
-
-});
--- dist/assets/js/all.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$(document).ready(function () {
-  $('.burger').click(function (e) {
-    e.preventDefault();
-    $('.menu').toggleClass('active');
-  });
-});
-//# sourceMappingURL=all.js.map
